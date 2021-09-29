9eb580a9359fb6b279ae9f8ff6b3106c38c491ed
Andy <andytcodes@gmail.com>
2021-09-28 21:23:31 -0400

    added login and registration modal

--- gesture/src/components/LandingPage.js
+++ gesture/src/components/LandingPage.js
@@ -25,9 +25,58 @@ const LandingPage = () => {
                 It's your time to sign! <br/> Begin your ASL journey on  
                 <em className="text-primary"> Gesture</em>! 👋
           </h1>
-          {/* Login & Register */}
-          <button className="btn btn-accent btn-wide mb-2" >Login</button>
-          <a className="link-primary block mb-10" href="registerpage">Register</a>
+          {/* Login Form */}
+          <a href="#login-form" className="btn btn-accent btn-wide mb-2" >Login</a>
+          <div id="login-form" class="modal">
+            <div class="modal-box">
+              <h1 className="text-primary font-bold text-3xl">Login</h1>
+              <form method="post" action="/">
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Email</span>
+                  </label> 
+                  <input placeholder="email" class="input input-bordered input-primary" type="text"/>
+                </div>
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Password</span>
+                  </label> 
+                  <input placeholder="password" class="input input-bordered input-primary" type="password"/>
+                </div>
+                <div class="modal-action">
+                    <button class="btn btn-primary" type="submit">Login</button> 
+                    <a href="/" class="btn">Close</a>
+                </div>
+              </form>
+            </div>
+          </div>
+          {/* Login Form End */}
+          {/* Register Form */}
+          <a href="#register-form" className="link-primary block mb-10">Register</a>
+          <div id="register-form" class="modal">
+            <div class="modal-box">
+              <h1 className="text-primary font-bold text-3xl">Register</h1>
+              <form method="post" action="/">
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Email</span>
+                  </label> 
+                  <input placeholder="email" class="input input-bordered input-primary" type="text"/>
+                </div>
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Password</span>
+                  </label> 
+                  <input placeholder="password" class="input input-bordered input-primary" type="password"/>
+                </div>
+                <div class="modal-action">
+                    <button class="btn btn-primary" type="submit">Register</button> 
+                    <a href="/" class="btn">Close</a>
+                </div>
+              </form>
+            </div>
+          </div>
+          {/* Register Form End */}
           {/* collapse content */}
           <div className="collapse collapse-arrow w-60 border rounded-box border-primary border-opacity-5 shadow-2xl" tabIndex="0">
             <input type="checkbox"/> 
